433bb03c89810d6b727b8a92369a5afed0974271
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-25 13:52:50 -0700

    Update Consecutive-strings.js

--- Consecutive-strings.js
+++ Consecutive-strings.js
@@ -22,3 +22,17 @@ Examples:
 
 // Solution
 
+function longestConsec(strarr, k) {
+  if (strarr.length === 0 || k > strarr.length || k <= 0) return "";
+  let result = '';
+  for (let i = 0; i <= strarr.length - k; i++) {
+    let str = '';
+    for (let j = i; j < i + k; j++) {
+      str += strarr[j];
+    }
+    if (result.length < str.length) {
+      result = str;
+    }
+  }
+  return result;
+}
